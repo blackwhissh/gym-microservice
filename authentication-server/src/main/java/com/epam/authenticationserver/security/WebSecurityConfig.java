f39b05cde516b141bab55afe6e90deb872ea3cdb
Nikoloz Kiladze <blackwhissh@gmail.com>
2024-05-22 16:53:43 +0400

    permit access on swagger endpoints

--- authentication-server/src/main/java/com/epam/authenticationserver/security/WebSecurityConfig.java
+++ authentication-server/src/main/java/com/epam/authenticationserver/security/WebSecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -41,7 +40,8 @@ public class WebSecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
 //                        .requestMatchers(EndpointRequest.to(HealthEndpoint.class)).permitAll()
-                                .requestMatchers("/error", "/v1/user/login/**").permitAll()
+                                .requestMatchers("/error", "/v1/user/login/**", "/swagger-ui/**",
+                                        "/swagger-ui.html", "/v3/api-docs/**").permitAll()
                                 .anyRequest().authenticated()
                 );
 
